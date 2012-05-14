8561163d8286922f94db947d8a1a0247abe8c7f0
developer <developer@ubuntu-station.(none)>
2012-05-14 15:25:10 +0200

    Latest Mathias comments are fixed

--- build.py
+++ build.py
@@ -1,12 +1,27 @@
 import subprocess
+import sys
 
-COMPILE_COMMAND = "/usr/texbin/pdflatex -interaction=nonstopmode report.tex"
+if sys.platform.startswith('darwin'):
+	COMPILE_COMMAND = "/usr/texbin/pdflatex -interaction=nonstopmode report.tex"
 
-process = subprocess.Popen(COMPILE_COMMAND, shell=True, stdout=subprocess.PIPE)
-for line in process.stdout:
-    print line
-process.wait()
+	process = subprocess.Popen(COMPILE_COMMAND, shell=True, stdout=subprocess.PIPE)
+	for line in process.stdout:
+	    print line
+	process.wait()
 
-OPEN_COMMAND = "open report.pdf"
+	OPEN_COMMAND = "open report.pdf"
 
-subprocess.Popen(OPEN_COMMAND, shell=True, stdout=subprocess.PIPE).wait()
+	subprocess.Popen(OPEN_COMMAND, shell=True, stdout=subprocess.PIPE).wait()
+
+elseif sys.platform.startswith('linux'):
+
+	COMPILE_COMMAND = "latex2pdf -o report.pdf report.tex"
+
+	process = subprocess.Popen(COMPILE_COMMAND, shell=True, stdout=subprocess.PIPE)
+	for line in process.stdout:
+	    print line
+	process.wait()
+
+	OPEN_COMMAND = "xdg-open report.pdf"
+
+	subprocess.Popen(OPEN_COMMAND, shell=True, stdout=subprocess.PIPE).wait()
